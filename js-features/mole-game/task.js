08dd870ccfc6da84d68cefb1143de449477ad403
eugenia-smith <ev-genius4@mail.ru>
2021-12-19 21:25:22 +0300

    Task completed

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -10,19 +10,31 @@ for (let index = 1; index <= 9; index++) {
     function getHole(index) {
         let clickedHole = document.getElementById(`hole${index}`)
         return clickedHole;
-        
+
     };
-    
+
     let hole = getHole(index);
-    
-    hole.onclick = function() {
-        if (hole.className.includes( 'hole_has-mole' )) {
+
+    hole.onclick = function () {
+        if (hole.className.includes('hole_has-mole')) {
             counterKilled++;
             killed.textContent = counterKilled;
         } else {
             counterLost++;
             lost.textContent = counterLost;
         }
+
+        if (killed.textContent === '10') {
+            alert("Вы победили!");
+            killed.textContent = 0;
+            lost.textContent = 0;
+        } else if (lost.textContent === '5') {
+            alert("Вы проиграли");
+            killed.textContent = 0;
+            lost.textContent = 0;
+
+        }
+
     }
 
 }
