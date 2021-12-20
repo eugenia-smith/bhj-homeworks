13f4653db3bc835b1bc8d699f54e9895ab16cf46
eugenia-smith <ev-genius4@mail.ru>
2021-12-20 22:47:52 +0300

    Task 1 (generally) completed

---
+++ element-search/menu/task.js
@@ -0,0 +1,24 @@
+'use strict';
+
+let menuLink = document.querySelectorAll('.menu__link');
+let linksArr = Array.from(menuLink);
+
+for (let i = 0; i < menuLink.length; i++) {
+    menuLink[i].onclick = function () {
+        let parentLi = menuLink[i].closest('li');
+        //console.log(parentLi);
+        let subMenu = parentLi.querySelector('.menu_sub');
+
+        if (subMenu !== null && subMenu.classList.contains('menu_active') == false) {
+            subMenu.classList.add('menu_active');
+            //console.log(subMenu);
+
+        } else {
+            subMenu.classList.remove('menu_active');
+
+        }
+
+        return false;
+    }
+}
+
