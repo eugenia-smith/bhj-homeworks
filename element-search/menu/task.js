4e03187cdcae26c3ed7818bf22b9dba1442a3aca
eugenia-smith <ev-genius4@mail.ru>
2022-01-04 16:49:38 +0300

    More editing

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -6,16 +6,14 @@ let linksArr = Array.from(menuLink);
 for (let i = 0; i < menuLink.length; i++) {
     menuLink[i].onclick = function () {
         let parentLi = menuLink[i].closest('li');
-        //console.log(parentLi);
         let subMenu = parentLi.querySelector('.menu_sub');
 
-        if (subMenu !== null && subMenu.classList.contains('menu_active') == false) {
-            subMenu.classList.add('menu_active');
-            //console.log(subMenu);
+        if (subMenu !== null) {
+            subMenu.classList.toggle('menu_active');
 
         } else {
-            subMenu.classList.remove('menu_active');
-
+            console.log('reload');
+            return true;
         }
 
         return false;
