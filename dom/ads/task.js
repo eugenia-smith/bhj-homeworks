66303b0e1bbb1c2ea02222d86e1c3ea0c4659675
eugenia-smith <ev-genius4@mail.ru>
2022-01-08 15:06:41 +0300

    Task completed

---
+++ dom/ads/task.js
@@ -0,0 +1,24 @@
+'use strict';
+
+//let advertisingArr = document.getElementsByClassName('rotator__case');
+//let activeElement = document.querySelector('.rotator__case_active');
+//let rotatorBox = document.querySelector('.rotator');
+// let advertising = document.querySelector('.rotator__case');
+
+let rotateAdvs = function () {
+    let activeElement = document.querySelector('.rotator__case_active');
+    let rotatorBox = document.querySelector('.rotator');
+
+    if (activeElement.nextElementSibling !== null) {
+        activeElement.classList.remove('rotator__case_active');
+        activeElement.nextElementSibling.classList.add('rotator__case_active');
+    } else {
+        rotatorBox.firstElementChild.classList.add('rotator__case_active');
+        activeElement.classList.remove('rotator__case_active');
+    }
+}
+
+setInterval(rotateAdvs, 1000);
+
+
+
