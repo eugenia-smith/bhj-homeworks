ade21c3d4680e8c24df76ca1be2d438122293d9c
eugenia-smith <ev-genius4@mail.ru>
2022-02-19 16:32:54 +0300

    'click' event added

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,31 +1,34 @@
 'use strict';
 
 let taskInput = document.getElementById('task__input');
-//let addBtn = document.getElementById('tasks__add');
+let addBtn = document.getElementById('tasks__add');
 let taskForm = document.getElementById('tasks__form');
 let toDoList = document.getElementById('tasks__list');
 
 function createTask(event) {
 
-    if (event.code == 'Enter' && taskInput.value !== '') {
+    if (event.code == 'Enter' || event.target == addBtn) {
+        event.preventDefault();
+        if (taskInput.value.trim() !== "") {
+            let newTask = document.createElement('div');
+            newTask.className = 'task';
+            newTask.innerHTML = '<div class="task__title"></div> <a href="#" class="task__remove">&times;</a>';
+            let taskTitle = newTask.querySelector('.task__title');
+            let removeButton = newTask.querySelector('.task__remove');
 
-        let newTask = document.createElement('div');
-        newTask.className = 'task';
-        newTask.innerHTML = '<div class="task__title"></div> <a href="#" class="task__remove">&times;</a>';
-        let taskTitle = newTask.querySelector('.task__title');
-        let removeButton = newTask.querySelector('.task__remove');
+            removeButton.onclick = function () {
+                newTask.remove();
+            }
 
-        removeButton.onclick = function () {
-            newTask.remove();
+            taskTitle.innerText = taskInput.value;
+            toDoList.insertAdjacentElement('afterbegin', newTask);
+            taskInput.value = "";
         }
-
-        taskTitle.innerText = taskInput.value;
-        toDoList.insertAdjacentElement('afterbegin', newTask);
-        taskInput.value = "";
     }
 }
 
 taskForm.addEventListener('keydown', createTask);
+taskForm.addEventListener('click', createTask)
 
 
 
